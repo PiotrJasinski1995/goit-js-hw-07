0142ee2c3f4c1c039afc02bce0bb7fcbbc78dfbf
PiotrJasinski1995 <piotr.jasinski1995@gmail.com>
2023-11-15 22:44:50 +0100

    feat: implement task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryList = document.querySelector("ul.gallery");
 
-console.log(galleryItems);
+const galleryMarkup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li>
+       <a class="gallery__item" href="${original}">
+         <img
+           class="gallery__image"
+           src="${preview}"
+           alt="${description}"
+         />
+       </a>
+     </li>`
+  )
+  .join("");
+
+galleryList.insertAdjacentHTML("beforeend", galleryMarkup);
+const gallery = new SimpleLightbox(`gallery, li > a`, {
+  captionsData: "alt",
+  captionDelay: 250,
+});
